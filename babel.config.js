aeee63e73ab2694ea5299df0be07f68d8c3bccf2
Trikolon <git@pbz.im>
2018-11-09 14:35:58 +0100

    Hide debug logging in production

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+const plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(['transform-remove-console', { exclude: ['error', 'warn', 'info'] }]);
+}
+
 module.exports = {
   presets: [
     '@vue/app',
   ],
+  plugins,
 };
